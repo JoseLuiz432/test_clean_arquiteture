82eacd38226e292b9eba009cada67f0b7f06b679
Jose Luiz Maciel Pimenta <joseluizmacielp@gmail.com>
2022-04-13 12:30:41 -0300

    tests

---
+++ test/application/use_cases/CreateLead.test.js
@@ -0,0 +1,29 @@
+const Lead = require('../../../lib/domain/Lead');
+const LeadRepository = require('../../../lib/domain/LeadRepository');
+const mockLeadRepository = new LeadRepository();
+const CreateLead = require('../../../lib/application/use_cases/CreateLead');
+
+test('should resolve with the newly persisted lead (augmented with an ID)', async () => {
+  // given
+  const persistedLead = new Lead(
+    123, 7557239024, 'John', 11999999999, 
+    'Rua teste de maio', 'john.doe@email.com', 
+    {'latitude': 120, 'longitude': 120}, 2
+    );
+  mockLeadRepository.persist = jest.fn(() => persistedLead);
+
+  // when
+  const lead = await CreateLead(
+    7557239024, 'John', 11999999999, 
+    'Rua teste de maio', 'john.doe@email.com', 
+    {'latitude': 120, 'longitude': 120}, 2, 
+    { leadRepository: mockLeadRepository });
+
+  // then
+  expect(mockLeadRepository.persist).toHaveBeenCalledWith(new Lead(
+    null, 7557239024, 'John', 11999999999, 
+    'Rua teste de maio', 'john.doe@email.com', 
+    {'latitude': 120, 'longitude': 120}, 2
+    ));
+  expect(lead).toEqual(persistedLead);
+});
